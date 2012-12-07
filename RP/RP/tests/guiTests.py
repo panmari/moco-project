2c8cc47e62cdb6b14ebbea2e9e9eff536f2671bc
panmari <smmuzi@gmail.com>
2012-12-07 22:28:29 +0100

    made test useful

--- RP/RP/tests/guiTests.py
+++ RP/RP/tests/guiTests.py
@@ -3,28 +3,15 @@ Created on Dec 2, 2012
 
 @author: mazzzy
 '''
-import unittest
 import gtk
 from RP.gui import gui
-
-class Test(unittest.TestCase):
-
-
-    def setUp(self):
-        self.gui_instance = gui()
-        self.gui_instance.pcap_file = "../../medienkomm"
-        self.gui_instance.main.show()
-        gtk.main()
-
-    def tearDown(self):
-        pass
-
-
-    def testName(self):
-        #todo: make some fake widget with file assigned?
-        self.gui_instance.file_chosen(None)
+import logging
 
 
 if __name__ == "__main__":
-    #import sys;sys.argv = ['', 'Test.testName']
-    unittest.main()
+    logging.basicConfig(level = logging.INFO)
+    gui_instance = gui()
+    gui_instance.pcap_file = "../../medienkomm"
+    gui_instance.main.show()
+    gtk.main()
+    gui_instance.start_parsing(None)
