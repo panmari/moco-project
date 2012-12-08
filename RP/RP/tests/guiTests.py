12817cb7562492d91d3b82cff965eb30371e1a65
Aaron Karper <akarper@students.unibe.ch>
2012-12-08 09:37:19 +0100

    Updated tests

--- RP/RP/tests/guiTests.py
+++ RP/RP/tests/guiTests.py
@@ -4,13 +4,13 @@ Created on Dec 2, 2012
 @author: mazzzy
 '''
 import gtk
-from RP.gui import gui
+from RP.gui import Gui
 import logging
 
 
 if __name__ == "__main__":
     logging.basicConfig(level = logging.INFO)
-    gui_instance = gui()
+    gui_instance = Gui()
     gui_instance.pcap_file = "../../medienkomm"
     gui_instance.main.show()
     gtk.main()
